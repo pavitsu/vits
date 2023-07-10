8730bd43dc75367e286c7f7d1dafc8e438ca8683
pavitsu <33056397+pavitsu@users.noreply.github.com>
2023-07-11 01:08:50 +0700

    Update setup.py
    fixed error "core" not found

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+from distutils.extension import Extension
 
 setup(
   name = 'monotonic_align',
-  ext_modules = cythonize("core.pyx"),
+  ext_modules = cythonize([Extension(name='core',sources=['core.pyx'])])
+,
   include_dirs=[numpy.get_include()]
 )
